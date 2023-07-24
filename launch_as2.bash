b21e6871da75a2e113db5e5f3e12a4796995e486
pariaspe <35956525+pariaspe@users.noreply.github.com>
2023-07-24 15:44:10 +0200

    Initial commit

---
+++ launch_as2.bash
@@ -0,0 +1,81 @@
+#!/bin/bash
+
+usage() {
+    echo "  options:"
+    echo "      -e: estimator_type, choices: [raw_odometry, mocap_pose, ground_truth]"
+    echo "      -r: record rosbag"
+    echo "      -t: launch keyboard teleoperation"
+    echo "      -n: drone namespace, default is drone0"
+}
+
+# Arg parser
+while getopts "se:rtn" opt; do
+  case ${opt} in
+    e )
+      estimator_plugin="${OPTARG}"
+      ;;
+    r )
+      record_rosbag="true"
+      ;;
+    t )
+      launch_keyboard_teleop="true"
+      ;;
+    n )
+      drone_namespace="${OPTARG}"
+      ;;
+    \? )
+      echo "Invalid option: -$OPTARG" >&2
+      usage
+      exit 1
+      ;;
+    : )
+      if [[ ! $OPTARG =~ ^[swrt]$ ]]; then
+        echo "Option -$OPTARG requires an argument" >&2
+        usage
+        exit 1
+      fi
+      ;;
+  esac
+done
+
+source utils/tools.bash
+
+# Shift optional args
+shift $((OPTIND -1))
+
+## DEFAULTS
+estimator_plugin=${estimator_plugin:="raw_odometry"}
+record_rosbag=${record_rosbag:="false"}
+launch_keyboard_teleop=${launch_keyboard_teleop:="false"}
+drone_namespace=${drone_namespace:="drone"}
+
+# Generate the list of drone namespaces
+drone_ns=()
+num_drones=1
+for ((i=0; i<${num_drones}; i++)); do
+  drone_ns+=("$drone_namespace$i")
+done
+
+for ns in "${drone_ns[@]}"
+do
+  tmuxinator start -n ${ns} -p tmuxinator/session.yml drone_namespace=${ns} estimator_plugin=${estimator_plugin} &
+  wait
+done
+
+if [[ ${estimator_plugin} == "mocap_pose" ]]; then
+  tmuxinator start -n mocap -p tmuxinator/mocap.yml &
+  wait
+fi
+
+if [[ ${record_rosbag} == "true" ]]; then
+  tmuxinator start -n rosbag -p tmuxinator/rosbag.yml drone_namespace=$(list_to_string "${drone_ns[@]}") &
+  wait
+fi
+
+if [[ ${launch_keyboard_teleop} == "true" ]]; then
+  tmuxinator start -n keyboard_teleop -p tmuxinator/keyboard_teleop.yml simulation=false drone_namespace=$(list_to_string "${drone_ns[@]}") &
+  wait
+fi
+
+# Attach to tmux session ${drone_ns[@]}, window 0
+tmux attach-session -t ${drone_ns[0]}:mission
---
+++ mission.py.py
@@ -0,0 +1,69 @@
+#!/bin/python3
+
+from time import sleep
+import rclpy
+from as2_python_api.drone_interface import DroneInterface
+
+
+def drone_run(drone_interface: DroneInterface):
+    """ Run the mission """
+
+    speed = 0.5
+    takeoff_height = 1.0
+    height = 1.0
+
+    sleep_time = 2.0
+
+    dim = 2.0
+    path = [
+        [dim, dim, height],
+        [dim, -dim, height],
+        [-dim, dim, height],
+        [-dim, -dim, height],
+        [0.0, 0.0, takeoff_height],
+    ]
+
+    print("Start mission")
+
+    ##### ARM OFFBOARD #####
+    print("Arm")
+    drone_interface.offboard()
+    sleep(sleep_time)
+    print("Offboard")
+    drone_interface.arm()
+    sleep(sleep_time)
+
+    ##### TAKE OFF #####
+    print("Take Off")
+    drone_interface.takeoff(takeoff_height, speed=1.0)
+    print("Take Off done")
+    sleep(sleep_time)
+
+    ##### GO TO #####
+    for goal in path:
+        print(f"Go to with path facing {goal}")
+        drone_interface.go_to.go_to_point_path_facing(goal, speed=speed)
+        print("Go to done")
+    sleep(sleep_time)
+
+    ##### LAND #####
+    print("Landing")
+    drone_interface.land(speed=0.3)
+    print("Land done")
+
+    drone_interface.disarm()
+
+
+if __name__ == '__main__':
+
+    rclpy.init()
+
+    uav = DroneInterface(drone_id="drone0", verbose=False)
+
+    drone_run(uav)
+
+    uav.shutdown()
+    rclpy.shutdown()
+
+    print("Clean exit")
+    exit(0)
