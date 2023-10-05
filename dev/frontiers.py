c33d70fcc3ff0aba89ada477ac80360293cc5bf2
pariaspe <pedro.ariasp@upm.es>
2023-10-05 17:19:19 -0500

    filtering map limits

--- dev/frontiers.py
+++ dev/frontiers.py
@@ -3,6 +3,9 @@ from __future__ import annotations
 import cv2
 import numpy as np
 
+SAFETY_DISTANCE = 0.3
+RESOLUTION = 0.1
+
 
 def random_color() -> tuple:
     """radom color tuple"""
@@ -13,17 +16,19 @@ def get_frontiers(img: np.array, area_thresh: int = 10) -> tuple[list, list]:
     """Get map frontiers"""
     unk_obs = np.copy(img)
     unk_obs[unk_obs == 128] = 0
-    unk_obs = cv2.erode(unk_obs, np.ones((3, 3)))
+    it = int(SAFETY_DISTANCE / RESOLUTION)
+    unk_obs = cv2.erode(unk_obs, np.ones([3*it, 3*it]))
 
     # https://docs.opencv.org/4.x/dd/d1a/group__imgproc__feature.html#ga04723e007ed888ddf11d9ba04e2232de
     edges = cv2.Canny(unk_obs, 250, 250)
 
     obstacles = np.copy(img)
     obstacles[obstacles == 128] = 255
-    drone_size = np.array([0.3/0.1] * 2).astype(int)
-    cv2.rectangle(obstacles, [69, 99]-drone_size,
-                  [69, 99] + drone_size, 0, -1)  # drone pose
-    obstacles = cv2.erode(obstacles, np.ones((3, 3)))
+    drone_size = np.array([it] * 2).astype(int)
+    # cv2.rectangle(obstacles, [69, 99]-drone_size,
+    #               [69, 99] + drone_size, 0, -1)  # drone pose
+    # one extra iteration to filter borders of map
+    obstacles = cv2.erode(obstacles, np.ones([3*(it+1), 3*(it+1)]))
 
     frontiers = cv2.bitwise_and(obstacles, edges)
 
