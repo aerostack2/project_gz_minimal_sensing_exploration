29d3934e184ce708fea4a1eeba1bf9906f4b9d3d
pariaspe <pedro.ariasp@upm.es>
2023-11-07 12:33:05 -0600

    right x axis with area in m2

--- scripts/viz_evaluation.py
+++ scripts/viz_evaluation.py
@@ -8,30 +8,37 @@ def read_log(log_file: str) -> tuple[list[float], list[float]]:
         timestamp_0, width, height, resolution = f.readline().split(" ")
         data = f.readlines()
     total = int(width) * int(height)
-    x = []
-    y = []
+    xdata = []  # time (s)
+    ydata = []  # area explored (%)
+    ydata_m2 = []  # area explored (m^2)
     for line in data:
         timestamp, _, unknown, *_ = line.split(" ")
         known = total - int(unknown[:-1])
-        x.append(float(timestamp) - float(timestamp_0))
-        y.append(100 * known / total)
+        xdata.append(float(timestamp) - float(timestamp_0))
+        ydata.append(100 * known / total)
+        ydata_m2.append(known * float(resolution) * float(resolution))
 
-    return x, y
+    return xdata, ydata, ydata_m2
 
 
 def plot_log(log_file):
     """Plot the log file"""
     # Data for plotting
-    t, s = read_log(log_file)
+    t, s, s2 = read_log(log_file)
 
     fig, ax = plt.subplots()
+    ax.set_title('Exploration results')
+    ax.set_xlabel('time (s)')
+    ax.set_ylabel('area (%)')
+    ax.grid()
     ax.plot(t, s)
 
-    ax.set(xlabel='time (s)', ylabel='area (%)',
-           title='Exploration results')
-    ax.grid()
+    ax2 = ax.twinx()
+    ax2.set_ylabel('area (m^2)')
+    ax2.plot(t, s2)
 
     # fig.savefig("test.png")
+    fig.tight_layout()
     plt.show()
 
 
